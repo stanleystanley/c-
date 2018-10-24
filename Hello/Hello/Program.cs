19d5330880809375a56ff7e6c813498ccb410c0a
schouten <test@test.nl>
2018-10-24 19:49:14 +0200

    First commit

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello," + args[1]);
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello" + name);
+            if(hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+
+            
+
+        }
+    }
+}
